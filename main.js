1c941ec6cd185fe214925a5add38821b8d53eacc
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-01-27 21:13:07 -0500

    if_else_elif_27_01_2021

---
+++ main.js
@@ -0,0 +1,12 @@
+let num1 = 49;
+let num2 = 49;
+
+if(num1==num2){
+    console.log(`El numero ${num1} es igual a ${num2}`);
+}else if (num1<num2){//
+
+    console.log(`El numero ${num1} es menor que ${num2}`);
+}
+else{
+    console.log(`Los numeros ${num1} y ${num2} no son iguales`);
+}
